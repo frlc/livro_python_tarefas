29b9d468918df7c984ff32d039e4eddf396825eb
frlc <frlc_31@hotmail.com>
2020-05-13 20:04:46 -0300

    aula regex

---
+++ main.py
@@ -0,0 +1,5 @@
+import re
+message = 'Call me at 415-555-1011 tomorrow. 415-555-9999 is my office.'
+phoneNumRegex = re.compile(r'\d\d\d-\d\d\d-\d\d\d\d')
+finded = phoneNumRegex.search(message)
+print(finded.group())
